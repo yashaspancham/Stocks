30e8429fbb2897d974d236cd9be9bb3f2c6409b8
yashaspancham <yashaspancham@gmail.com>
2023-07-25 23:10:59 +0530

    code cleaned

--- Bro'sScan.py
+++ Bro'sScan.py
@@ -1,17 +1,17 @@
 import pandas
+import tools
 import time
 from bs4 import BeautifulSoup
 
 start_timer = time.time()
-stock_list = pandas.read_csv("/home/yashas/Desktop/Quant/NET_NET/Equity.csv")
-Security_Code_list = stock_list.index.to_list()
+Security_Code_list =tools.stock_security_list() 
 count = 0
 filed_count=0
 final_list = []
 
 for security_code in Security_Code_list:
     print(security_code)
-    if count == 10000:
+    if count == 100000:
         break
     security_code = str(security_code)
     try:
@@ -23,33 +23,9 @@ for security_code in Security_Code_list:
         sales = sales[-5:]
         OPMp = OPMp[-5:]
         
+        sales_flag=tools.sales_check(sales)
 
-        # To check if sales condition is met
-        sales_flag = 1
-        for i in range(len(sales) - 1):
-            sales[i]=str(sales[i])
-            if "," in sales[i]:
-                sales[i]=sales[i].replace(",","")
-                
-            if float(sales[i]) == 0:
-                #print(sales[i])
-                continue
-            else:
-                if ((float(sales[i + 1]) - float(sales[i])) / float(sales[i])) * 100 < 12:
-                    sales_flag = 0
-                    break
-
-        # To check if OPM% condition is met
-        OPMp_flag = 1
-        for i in range(len(OPMp) - 1):
-            OPMp[i] = str(OPMp[i]).replace("%", "")
-            if "," in OPMp[i]:
-                OPMp[i]=OPMp[i].replace(",","")
-            if OPMp[i]=='nan':
-                OPMp[i]=0    
-            if float(OPMp[i]) < 10:
-                OPMp_flag = 0
-                break
+        OPMp_flag=tools.OPMp_check(OPMp)
 
         # To check if P/E condition is met
         with open("/home/yashas/Desktop/Quant/DATA/" + security_code + "/" + security_code + ".html", "r") as file:
@@ -60,14 +36,8 @@ for security_code in Security_Code_list:
         for number in numbers:
             if "Stock P/E" in number.text:
                 stock_PtoE = number.find('span', class_='number').text
-        if "," in stock_PtoE:
-            stock_PtoE=stock_PtoE.replace(",","")
-        PtoE_flag = 0
-        if stock_PtoE=="":
-            PtoE_flag=0
-        else:
-            if float(stock_PtoE) <= 20:
-                PtoE_flag = 1
+        PtoE_flag=tools.Stock_PtoE_check(stock_PtoE)
+
         if PtoE_flag == 1 and sales_flag == 1 and OPMp_flag == 1:
             final_list.append(security_code)
             print(final_list)
---
+++ tools.py
@@ -0,0 +1,44 @@
+import pandas
+def stock_security_list():
+    stock_list = pandas.read_csv("/home/yashas/Desktop/Quant/NET_NET/Equity.csv")
+    Security_Code_list = stock_list.index.to_list()
+    return Security_Code_list
+
+def sales_check(sales):#To check if last 5years of sales compounds at 12%
+    sales_flag = 1
+    for i in range(len(sales) - 1):
+        sales[i]=str(sales[i])
+        if "," in sales[i]:
+            sales[i]=sales[i].replace(",","")
+            
+        if float(sales[i]) == 0:
+            continue
+        else:
+            if ((float(sales[i + 1]) - float(sales[i])) / float(sales[i])) * 100 < 12:
+                sales_flag = 0
+                break
+    return sales_flag
+
+def OPMp_check(OPMp):#To check if OPMp>10% for last 5 yers
+    OPMp_flag = 1
+    for i in range(len(OPMp) - 1):
+        OPMp[i] = str(OPMp[i]).replace("%", "")
+        if "," in OPMp[i]:
+            OPMp[i]=OPMp[i].replace(",","")
+        if OPMp[i]=='nan':
+            OPMp[i]=0    
+        if float(OPMp[i]) < 10:
+            OPMp_flag = 0
+            break
+    return OPMp_flag
+
+def Stock_PtoE_check(stock_PtoE):#to check P/E<=20(currently)
+    if "," in stock_PtoE:
+        stock_PtoE=stock_PtoE.replace(",","")
+    PtoE_flag = 0
+    if stock_PtoE=="":
+        PtoE_flag=0
+    else:
+        if float(stock_PtoE) <= 20:
+            PtoE_flag = 1
+    return PtoE_flag
